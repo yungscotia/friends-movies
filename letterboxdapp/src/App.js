c24e375e6383970efe79a000ba9f4cb15613f768
Nova <nova697@Novas-MacBook-Pro.local>
2020-06-29 20:49:11 -0700

    added a basic frontend and now working on web scraping

--- letterboxdapp/src/App.js
+++ letterboxdapp/src/App.js
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
+import arrowRight from './assets/arrowright.svg';
 import './App.css';
 
 function App() {
+  const [username, setValue] = React.useState('');
+  const [usernames, setUsername] = React.useState([]);
+  const [data, setData] = React.useState();
+  const [header, setHeader] = React.useState(true);
+  const [main, setMain] = React.useState(false);
+  let headerClass;
+  let mainClass;
+
+  headerClass = header ? 'App-header':'hideHeader';
+  mainClass = main ? 'mainPage' : 'hideMain';
+
+  function handleHeader(event) {
+    setHeader(false);
+    setMain(true);
+  };
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    if(username) {
+      setUsername(usernames.concat(username));
+    }
+
+    event.preventDefault();
+    setValue('');
+  }
+
+  function handleClear(event) {
+    setUsername([]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={headerClass}>
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to <a href="fleapit.com/about">Fleapit</a></h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Find movies to watch with your friends using your <a href="https://letterboxd.com/" target="_blank"><code>Letterboxd</code></a> username!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="getstarted" onClick={handleHeader}>
+          <a>Get Started</a>
+          <img src={arrowRight} />
+        </div>
       </header>
+      <div className={mainClass}>
+        <form onSubmit={handleSubmit}>
+          <input className="inputbox" type="text" value={username} onChange={handleChange} />
+          <button type="submit" className="button"> Add Username </button>
+        </form>
+        <button type="clear" className="button" onClick={handleClear}>clear</button>
+        <ul className="no-bullets">
+          {usernames.map(item => (
+            <li className="username" key={item}> <a href={"https://letterboxd.com/" + item} target="_blank">{item}</a> </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
