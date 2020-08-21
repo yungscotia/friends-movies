55c6ffb4b201adb7810f26c0e41d690c859e9a2f
Nova <nova697@Novas-MacBook-Pro.local>
2020-08-21 14:41:38 -0700

    Fixed an issue with the querySelector and cleanedup code

--- letterboxdapp/server/Database_Builders/getFilmDetailsByTMDBID.js
+++ letterboxdapp/server/Database_Builders/getFilmDetailsByTMDBID.js
@@ -1,7 +1,8 @@
 const fetch = require('isomorphic-fetch');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const filmData = require('./allTMDBMovies.json');
+let filmData = require('./allTMDBMovies.json');
+filmData = filmData.slice(11, 22);
 
 function createTMDB_API_URL(id) {
     const APIkey = '20fbcd49dc115cbc2807646f1aa53b83';
@@ -16,8 +17,7 @@ function createLetterboxdLink(id) {
 
 function getFilmDetailsByID(id) {
     let APIurl = createTMDB_API_URL(id);
-    return fetch(APIurl)
-        .then(response => response.json());
+    return fetch(APIurl).then(response => response.json());
 }
 
 function prepTMDBMoviesData(filmData) {
@@ -28,25 +28,23 @@ function prepTMDBMoviesData(filmData) {
 }
 
 async function getAllFilmDetails(filmData) { //This is where the magic happens!
-    await Promise.all(filmData.map(async filmID => {
-        let details = await getFilmDetailsByID(filmID);
-        return details;
+    return Promise.all(filmData.map(async filmID => {
+        return getFilmDetailsByID(filmID);
     }))
-
-    return filmData;
 }
 
-async function getLetterboxdRating(id) {
+async function getLetterboxdRating(id, browser) {
     let letterboxd_url = createLetterboxdLink(id);
-    const browser = await puppeteer.launch({
-        headless: true
-    });
-    const page = browser.newPage();
+    const page = await browser.newPage();
     await page.goto(letterboxd_url, {waitUntil: 'networkidle2'});
     let rating = await page.evaluate(function() {
-        return parseFloat(document.querySelector('span.average-rating').querySelector('a').innerText);
+        if(document.querySelector('span.average-rating') != null) {
+            return parseFloat(document.querySelector('span.average-rating').querySelector('a').innerText);
+        } else {
+            return null;
+        }
     });
-
+    await page.close();
     return rating;
     //finish by fetching and parsing HTML letterboxd Data from fetch(letterboxd_url)
 }
@@ -54,18 +52,23 @@ async function getLetterboxdRating(id) {
 async function getLetterboxdRatings(filmData) {
     filmData = prepTMDBMoviesData(filmData);
     filmData = await getAllFilmDetails(filmData);
+    const browser = await puppeteer.launch({
+        headless: true
+    });
     await Promise.all(filmData.map(async filmDetails => {
         let id = filmDetails.id;
-        let letterboxd_rating = await getLetterboxdRating(id);
-        filmDetails['avg_letterboxd_rating'] = letterboxd_rating;
+        console.log(id);
+        filmDetails['avg_letterboxd_rating'] = await getLetterboxdRating(id, browser);
     }))
-
-    fs.writeFile('fullMovieDatabase.json', filmData, (err) => {
+    console.log(filmData);
+    fs.writeFile('fullMovieDatabase.json', JSON.stringify(filmData), (err) => {
         if(err) {
             throw(err);
         }
         console.log('full movie database created and saved at fullMovieDatabase.json');
     })
+    await browser.close();
+    return console.log('all done!');
 }
 
 // READ HERE!!
@@ -78,4 +81,4 @@ async function getLetterboxdRatings(filmData) {
 //If you have a proper JSON file of all the TMDB data from the above step
 //then simply run getLetterboxdRatings(filmData) where filmData is the file of all the TMDB movies imported on line 3 of this file
 
-//getLetterboxdRatings(filmData);
+getLetterboxdRatings(filmData);
