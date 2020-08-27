670de7dc7b817ab659952f777cbb6fd3753d7592
Nova <nova697@Novas-MacBook-Pro.local>
2020-08-27 16:39:20 -0700

    modularized the userTab component

--- letterboxdapp/src/componenets/sidebar.js
+++ letterboxdapp/src/componenets/sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {bubble as Menu} from 'react-burger-menu';
+import UserTab from './userTab.js';
 
 class SideBar extends React.Component {
     constructor(props) {
@@ -120,12 +121,7 @@ class SideBar extends React.Component {
         return (
             <Menu pageWrapId={this.props.pageWrapId} outerContainerId={this.props.outerContainerId} isOpen={this.props.shouldMenuOpen} customCrossIcon={false} disableAutoFocus disableCloseOnEsc disableOverlayClick noOverlay>
                 {this.props.users.map(user => (
-                    <div>
-                        <svg width="1.5vw" height="1.5vw" viewBox="0 0 16 16" className="bi bi-x-square-fill" id="adduser" fill="currentColor" xmlns="http://www.w3.org/2000/svg" onClick={() => this.removeUser(user)}>
-                            <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm9.854 4.854a.5.5 0 0 0-.708-.708L8 7.293 4.854 4.146a.5.5 0 1 0-.708.708L7.293 8l-3.147 3.146a.5.5 0 0 0 .708.708L8 8.707l3.146 3.147a.5.5 0 0 0 .708-.708L8.707 8l3.147-3.146z"/>
-                        </svg>
-                        <a id="username" className="menu-item" onClick={() => this.changeActiveTab(user)}>{user}</a>
-                    </div>
+                    <UserTab removeUser={this.removeUser} changeActiveTab={this.changeActiveTab} user={user}></UserTab>
                 ))}
                 {addUserForm}
             </Menu>
--- letterboxdapp/src/componenets/userTab.js
+++ letterboxdapp/src/componenets/userTab.js
@@ -10,20 +10,34 @@ class UserTab extends React.Component {
 
         this.removeUser = this.removeUser.bind(this);
         this.changeActiveTab = this.changeActiveTab.bind(this);
+        this.buttonChange = this.buttonChange.bind(this);
+    }
+
+    removeUser(user) {
+        this.props.removeUser(user);
+    }
+
+    changeActiveTab(user) {
+        this.props.changeActiveTab(user);
+    }
+
+    buttonChange() {
+        this.setState({buttonFill: !this.state.buttonFill});
     }
 
 
     render() {
         let button;
+        let user = this.props.user;
         if(this.state.buttonFill) {
             button = ( 
-                <svg width="1.5vw" height="1.5vw" viewBox="0 0 16 16" className="bi bi-x-square-fill" id="adduser" fill="currentColor" xmlns="http://www.w3.org/2000/svg" onClick={() => this.removeUser(user)}>
+                <svg width="1.5vw" height="1.5vw" viewBox="0 0 16 16" className="bi bi-x-square-fill" id="adduser" fill="currentColor" xmlns="http://www.w3.org/2000/svg" onClick={() => this.removeUser(user)} onMouseLeave={this.buttonChange}>
                     <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm9.854 4.854a.5.5 0 0 0-.708-.708L8 7.293 4.854 4.146a.5.5 0 1 0-.708.708L7.293 8l-3.147 3.146a.5.5 0 0 0 .708.708L8 8.707l3.146 3.147a.5.5 0 0 0 .708-.708L8.707 8l3.147-3.146z"/>
                 </svg>
             );
         } else {
             button = ( 
-                <svg width="1.5vw" height="1.5vw" viewBox="0 0 16 16" className="bi bi-x-square" id="adduser" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <svg width="1.5vw" height="1.5vw" viewBox="0 0 16 16" className="bi bi-x-square" id="adduser" fill="currentColor" xmlns="http://www.w3.org/2000/svg" onMouseEnter={this.buttonChange}>
                     <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                     <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
                     <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
@@ -31,7 +45,7 @@ class UserTab extends React.Component {
             );
         }
         return ( 
-            <div>
+            <div className="bm-item" >
                 {button}
                 <a id="username" className="menu-item" onClick={() => this.changeActiveTab(user)}>{user}</a>
             </div>
